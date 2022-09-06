9e28360f9a3280ceab7c0c145f823bc66e97ccd8
ankye <shengxiehua@gmail.com>
2022-09-06 14:47:16 +0800

    更新协议，调整结构

--- gen.sh
+++ gen.sh
@@ -6,4 +6,5 @@ mkdir -p $basepath/s2s
 protoc -I $basepath --go_out=plugins=grpc:./s2s $basepath/protofiles/s2s.proto
 mkdir -p $basepath/ss
 protoc -I $basepath --go_out=plugins=grpc:./ss $basepath/protofiles/ss.proto
-
+mkdir -p $basepath/s2s
+protoc -I $basepath --go_out=plugins=grpc:./s2s $basepath/app/user/s2suser.proto
